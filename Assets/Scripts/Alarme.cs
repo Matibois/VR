f30631d97aa8c55f788cf872b31a374186be2b0e
Matibois <100830614+Matibois@users.noreply.github.com>
2024-03-08 17:53:09 +0100

    begin alarme

---
+++ Assets/Scripts/Alarme.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Alarme : MonoBehaviour
+{
+    [SerializeField] private InputField InputFieldCode;
+    private string codeString;
+    private int code;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestCode()
+    {
+        codeString = InputFieldCode.GetComponent<InputField>().text;
+        code = Int32.Parse(codeString)
+;    }
+
+}
