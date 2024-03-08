0104f651aae4cce5a6b44cf6125c167b1da8afb1
Lyna <91870976+Sasha697@users.noreply.github.com>
2024-03-08 13:08:22 +0100

    still in progress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,9 +10,13 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private Timer _timer;
 
+    public UnityEvent onObjectiveCompleted = new UnityEvent();
+
+    // Player Objectives
     private bool _hasEnteredJewelry = false;
     private bool _hasDisarmedAlarm = false;
     private bool _hasFled = false;
+    private string _currentObjective = "Entrer dans la bijouterie";
 
     private void Awake()
     {
@@ -36,18 +41,33 @@ public class GameManager : MonoBehaviour
 
     }
 
+
     public void EnterJewelry()
     {
         _hasEnteredJewelry = true;
+        _currentObjective = "Désarmer l'alarme";
+        onObjectiveCompleted.Invoke();
     }
 
-    public void DisarmAlarm() 
-    { 
-        _hasDisarmedAlarm = true; 
+
+    public void DisarmAlarm()
+    {
+        _hasDisarmedAlarm = true;
+        _currentObjective = "Vol des bijoux";
+        onObjectiveCompleted.Invoke();
     }
 
-    public void Flee() 
-    { 
-        _hasFled = true; 
+
+    public void Flee()
+    {
+        _hasFled = true;
+        _currentObjective = "Objectif atteint";
+        onObjectiveCompleted.Invoke();
+    }
+
+
+    public string GetCurrentObjective()
+    {
+        return _currentObjective;
     }
 }
